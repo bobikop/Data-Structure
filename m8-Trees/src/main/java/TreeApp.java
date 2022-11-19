fd770b5879a3160892a08ea14b87f01f5b51a292
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-11-18 22:08:57 -0500

    Data Structures

--- m8-Trees/src/main/java/TreeApp.java
+++ m8-Trees/src/main/java/TreeApp.java
@@ -19,11 +19,13 @@ public class TreeApp {
         System.out.println("Level Order Traversal: ");
         tree.levelOrderTraversal();*/
 
-            tree.printLeaves(tree.root);
-            System.out.println();
-            System.out.println("Number of leaves:  " +tree.countLeaves(tree.root));
-            System.out.println("Sum of Leaf Nodes: " + tree.findSumOfLeaves(tree.root));
-            System.out.println(tree.height(tree.root));
+        tree.printLeaves(tree.root);
+        System.out.println();
+        System.out.println("Number of leaves:  " +tree.countLeaves(tree.root));
+        System.out.println("Sum of Leaf Nodes: " + tree.findSumOfLeaves(tree.root));
+        System.out.println("Height of the tree is: "+ tree.height(tree.root));
+        System.out.println("Sum of Node Depths is: " + tree.calculateNodeDepthSums());
+        System.out.println("Sum of all node values is: " + tree.calculateNodeSums());
         }
 }
 
