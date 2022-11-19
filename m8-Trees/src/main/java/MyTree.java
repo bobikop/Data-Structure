ed207a8ee18b6d97c62b2a271974f72170b0c91b
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-11-18 22:09:13 -0500

    Data Structures

--- m8-Trees/src/main/java/MyTree.java
+++ m8-Trees/src/main/java/MyTree.java
@@ -6,81 +6,88 @@ public class MyTree {
 
     public MyTree() {
     }
-    void insert(int value){
-        TNode newNode=new TNode(value);
-        if (root==null) {
-            root=newNode;
+
+    void insert(int value) {
+        TNode newNode = new TNode(value);
+        if (root == null) {
+            root = newNode;
             return;
         }
-        TNode current =root;
-        while(true){
-            if (value<= current.value){
+        TNode current = root;
+        while (true) {
+            if (value <= current.value) {
 
-                if (current.leftChild==null){
+                if (current.leftChild == null) {
                     // if left is null insert there!!!
-                    current.leftChild=newNode;
+                    current.leftChild = newNode;
                     break;
                 }
                 //if leftChild is not null branch into left subtree!!
-                current=current.leftChild;
-            }
-            else {
-                if (current.rightChild==null){
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
                     // if right is null insert there!!!
-                    current.rightChild=newNode;
+                    current.rightChild = newNode;
                     break;
                 }
                 //if rightChild is not null branch into right subtree!!
-                current=current.rightChild;
+                current = current.rightChild;
             }
         }
     }
+
     // PreOrder Traversal of the tree
     // Root-Left-right
-    void preOrderTraversal(TNode root){
-        if(root==null) return;// termination
-        System.out.print(root.value +", "); // visit root
+    void preOrderTraversal(TNode root) {
+        if (root == null) return;// termination
+        System.out.print(root.value + ", "); // visit root
         preOrderTraversal(root.leftChild);  // visit left subtree
         preOrderTraversal(root.rightChild); // visit right subtree
     }
-    void inOrderTraversal(TNode root){
-        if (root==null) return; // termination
+
+    void inOrderTraversal(TNode root) {
+        if (root == null) return; // termination
         inOrderTraversal(root.leftChild);
-        System.out.print(root.value+", ");
+        System.out.print(root.value + ", ");
         inOrderTraversal(root.rightChild);
     }
-    void postOrderTraversal(TNode root){
-        if (root==null) return;// termination
+
+    void postOrderTraversal(TNode root) {
+        if (root == null) return;// termination
         postOrderTraversal(root.leftChild); // branch and finish left subtree
         postOrderTraversal(root.rightChild); // branch and finish right subtree
-        System.out.print(root.value+", ");  // visit root
+        System.out.print(root.value + ", ");  // visit root
     }
-    void levelOrderTraversal(){
-        if(root==null) return;
-        Queue<TNode>  queue=new LinkedList<>();
+
+    void levelOrderTraversal() {
+        if (root == null) return;
+        Queue<TNode> queue = new LinkedList<>();
         queue.add(root);
-        while(!queue.isEmpty()){
-            TNode toVisit=queue.poll();
-            System.out.print(toVisit.value+", ");
-            if (toVisit.leftChild!=null) queue.add(toVisit.leftChild);
-            if (toVisit.rightChild!=null) queue.add(toVisit.rightChild);
+        while (!queue.isEmpty()) {
+            TNode toVisit = queue.poll();
+            System.out.print(toVisit.value + ", ");
+            if (toVisit.leftChild != null) queue.add(toVisit.leftChild);
+            if (toVisit.rightChild != null) queue.add(toVisit.rightChild);
         }
     }
-    public boolean contains(int value){
-        if (root==null) return false;
-        TNode current= root;
-        while (current!=null) {
-            if (value<current.value) current=current.leftChild;
-            else if (value> current.value) current=current.rightChild;
+
+    public boolean contains(int value) {
+        if (root == null) return false;
+        TNode current = root;
+        while (current != null) {
+            if (value < current.value) current = current.leftChild;
+            else if (value > current.value) current = current.rightChild;
             else return true;
         }
         return false;
     }
+
     public boolean isLeaf(TNode node) {
-        return node.leftChild==null && node.rightChild==null;
+        return node.leftChild == null && node.rightChild == null;
     }
-    public void printLeaves(TNode root){
-        if (root==null) return;
+
+    public void printLeaves(TNode root) {
+        if (root == null) return;
         // perform visit on Root
 
         // Recursively Branch Left Subtree
@@ -90,21 +97,48 @@ public class MyTree {
 
         // Recursively Branch Right Subtree
     }
-    int countLeaves(TNode root){
-        if (root==null) return 0;
+
+    int countLeaves(TNode root) {
+        if (root == null) return 0;
         if (isLeaf(root)) return 1;
         // recursively left
         // recursively right
         return countLeaves(root.leftChild) + countLeaves(root.rightChild);
     }
-    int findSumOfLeaves(TNode root){
-        if (root==null) return 0;
+
+    int findSumOfLeaves(TNode root) {
+        if (root == null) return 0;
         if (isLeaf(root)) return root.value;
         return findSumOfLeaves(root.leftChild) + findSumOfLeaves(root.rightChild);
     }
-    int height(TNode root){
-        if (root==null) return -1;
+
+    int height(TNode root) {
+        if (root == null) return -1;
         if (isLeaf(root)) return 0;
-        return 1+ Math.max(height(root.leftChild), height(root.rightChild));
+        return 1 + Math.max(height(root.leftChild), height(root.rightChild));
     }
+
+
+    public int calculateNodeDepthSums(){
+        return nodeDepthSums(root, 0);
+    }
+
+    // Task -sum of node depths
+
+    // Assignment  Sum of Node Depths
+    public int nodeDepthSums(TNode node, int A){
+        if(node==null) return 0;
+        System.out.println(A);
+        return A+nodeDepthSums(node.leftChild, A+1) +nodeDepthSums(node.rightChild, A+1);
+    }
+    // Assignment  Sum of All Nodes
+    public int calculateNodeSums(){
+        return nodeSums(root, root.value);
+    }
+    // Assignment  Sum of Node Depths
+    public int nodeSums(TNode node, int sum){
+        if(node==null) return 0;
+        return sum+nodeSums(node.leftChild, node.value) +nodeSums(node.rightChild, node.value);
+    }
+    // Assignment  Sum of all Nodes
 }
---
+++ m9-Heap/src/main/java/HeapApp.java
@@ -0,0 +1,18 @@
+public class HeapApp {
+
+
+    public static void main(String[] args) {
+
+        MyHeap heap = new MyHeap(10);
+        heap.insert(61);
+        heap.insert(120);
+        heap.insert(52);
+        heap.insert(45);
+        heap.insert(81);
+       heap.printHeap();
+
+
+
+
+    }
+}
