340a40221d02b767f9e67d1732d9220434be0ac4
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-11 13:30:47 -0400

    Data Structures

--- m3-CollectionsReview/src/main/java/Student.java
+++ m3-CollectionsReview/src/main/java/Student.java
@@ -3,12 +3,10 @@ import java.util.Objects;
 public class Student {
     int id;
     String name;
-
     public Student(int id, String name) {
         this.id = id;
         this.name = name;
     }
-
     public int getId() {
         return id;
     }
